8dcaf74e398c26c0c0e95e1ba071dc1861fe25de
ashraf4663 <ashraf4663@gmail.com>
2021-06-14 03:38:16 +0600

    invent 1st c done

---
+++ app/Http/Controllers/firstController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class firstController extends Controller
+{
+    public function Index(){
+        echo "done";
+    }
+}
